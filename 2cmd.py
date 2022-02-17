a2b9f28ba9b07e51f15f1a16b6844f0650f3f849
Zarcolio <registered@quicknet.nl>
2022-02-17 19:33:52 +0100

    Added -hh to show help text inside .2cmd file if present

--- 2cmd.py
+++ 2cmd.py
@@ -58,7 +58,8 @@ signal.signal(signal.SIGINT, signal_handler)
 # Get some commandline arguments:
 parser = argparse.ArgumentParser(description="This script takes input lines from stdin and inserts them in the commands provided in the commands file. This way you can execute a certain command many times. For example you can take screen shots of URLs with cutycapt provided by output of another command.")
 parser.add_argument("cmd", help="File containing one or more commands that should be executed. If no path is provided, a file in scriptdir/2cmd.xmpls is assumed. Use $2cmd$ or $2cmdsan$ in lowercase in each command line. $2cmd$ is replaced with each line from input. Use $cmdsan$ to sanitize a string for use in a filename.")
-parser.add_argument("-2", "--second", help="Pass a second variable to the script to run. This goes into variable $2nd$")
+parser.add_argument("-hh", "--help2", help="Show the help inside the .2cmd script being called. Lines in the beginning of the script starting with # are displayed as help.", action="store_true")
+parser.add_argument("-2", "--second", help="Pass a second variable to the script to run. This goes into variable $2nd$.")
 parser.add_argument("-t", "--timeout", help="Wait x milliseconds between commands.")
 parser.add_argument("-v", "--verbose", help="In green, show the commands that are created from stdin and the provide config file.", action="store_true")
 parser.add_argument("-w", "--workers", help="Defines how many worker threads execute the commands parallelly.", default=1)
@@ -78,6 +79,9 @@ except FileNotFoundError:
 
 iFirst = 0
 pool = multiprocessing.Pool(int(args.workers))
+boolOutsideHelp = False
+boolHelpFound = False
+boolNoHelpPrinted = False
  
 for strInput in sys.stdin:
     if args.timeout:
@@ -90,17 +94,32 @@ for strInput in sys.stdin:
         if not strInput.strip():
             continue
         
-        strInputSan = FileNameSan(strInput)
-        sCmd = sCmd.replace("$2cmdsan$", strInputSan, len(sCmd))
-
-        sCmd = sCmd.replace("$2cmd$", strInput, len(sCmd))
-        if args.second:
-            sCmd = sCmd.replace("$2nd$", args.second, len(sCmd))
-
-        sCmd = sCmd.replace("\n", "", len(sCmd))
-        sCmd = escapeString(sCmd)
-        
-        pool.apply_async(execCmd, args = (sCmd, ))
+        if args.help2:
+            
+            if len(sCmd) > 0:
+                if sCmd[0] == "#" and boolOutsideHelp == False:
+                    print (sCmd)
+                    boolHelpFound = True
+                else:
+                    boolOutsideHelp = True
+                    if sCmd[0] != "#" and boolHelpFound == False and boolNoHelpPrinted == False:
+                        print("No help comment found in " + getFullDir(args.cmd))
+                        boolNoHelpPrinted = True
+        else:
+           if len(sCmd) > 0:
+                if sCmd[0] != "#":
+                    
+                    strInputSan = FileNameSan(strInput)
+                    sCmd = sCmd.replace("$2cmdsan$", strInputSan, len(sCmd))
+            
+                    sCmd = sCmd.replace("$2cmd$", strInput, len(sCmd))
+                    if args.second:
+                        sCmd = sCmd.replace("$2nd$", args.second, len(sCmd))
+            
+                    sCmd = sCmd.replace("\n", "", len(sCmd))
+                    sCmd = escapeString(sCmd)
+                    
+                    pool.apply_async(execCmd, args = (sCmd, ))
 
 pool.close()
 pool.join()
